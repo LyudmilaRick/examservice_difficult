a2ef1136afcb7412459c7487f64284009b2a9272
RickLyudmila <Lrick@yandex.ru>
2022-02-06 14:59:17 +0300

    First commit for the second level

---
+++ src/test/java/pro/sky/java/course2/level2/examservice_difficult/ExamserviceDifficultApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.level2.examservice_difficult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamserviceDifficultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
